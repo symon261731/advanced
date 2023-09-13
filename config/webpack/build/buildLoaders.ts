7021c670a900d36cba82f0e6e80bfebd47461906
Simon Mishchenko <symon261731@gmail.com>
2023-09-13 19:22:14 +0300

    добавлен babel с плагином для i18next

--- config/webpack/build/buildLoaders.ts
+++ config/webpack/build/buildLoaders.ts
@@ -5,6 +5,27 @@ import { BuildOptions } from './types/config';
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
 
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
+    };
+
     const tsLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -47,6 +68,7 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     }
 
     return [
+        babelLoader,
         tsLoader,
         scssLoader,
         svgLoader,
