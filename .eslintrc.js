e0ccae9eaccd5fc52bdee5cedced4b141c56c89a
Simon Mishchenko <symon261731@gmail.com>
2023-10-10 01:09:59 +0300

    добавлен игнор i18n для data-testid

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,6 +41,6 @@ module.exports = {
         'import/extensions': 0,
         'no-unused-vars': 1,
         'no-shadow': 0,
-        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid'] }],
     },
 };
