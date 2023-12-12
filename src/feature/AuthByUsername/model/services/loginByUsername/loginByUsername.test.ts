b0c7162ecc01b66de7586be6235ed04a1eccf8bc
Simon Mishchenko <symon261731@gmail.com>
2023-12-13 02:27:04 +0300

    сделаны изменения в классе TestAsyncThunk

--- src/feature/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
+++ src/feature/AuthByUsername/model/services/loginByUsername/loginByUsername.test.ts
@@ -1,13 +1,8 @@
-import axios from 'axios';
 import { Dispatch } from '@reduxjs/toolkit';
 import { IStateSchema } from 'app/providers/StoreProvider';
 import { userActions } from 'enteties/User';
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
 import { loginByUsername } from './loginByUsername';
-
-jest.mock('axios');
-
-const mockedAxios = jest.mocked(axios, true);
 
 describe('loginByUsername.test', () => {
     let dispatch: Dispatch;
@@ -19,23 +14,24 @@ describe('loginByUsername.test', () => {
     });
     test('sucess situation', async () => {
         const userValue = { username: '123', id: '1' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
 
         const thunk = new TestAsyncThunk(loginByUsername);
+
+        thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }));
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toEqual(userValue);
     });
 
     test('unsucess situation', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const thunk = new TestAsyncThunk(loginByUsername);
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const result = await thunk.callThunk({ username: '123', password: '123' });
 
-        expect(mockedAxios.post).toHaveBeenCalled();
+        // expect(mockedAxios.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toBe('error');
     });
